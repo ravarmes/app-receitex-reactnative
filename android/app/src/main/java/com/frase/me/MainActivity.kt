96371584172a346685919bd2e16d0c4500a2b7ae
Rafael Vargas Mesquita <ravarmes@hotmail.com>
2026-03-31 14:20:57 -0300

    bump version to 1.0.1 and add edge-to-edge support for Android 15

--- android/app/src/main/java/com/frase/me/MainActivity.kt
+++ android/app/src/main/java/com/frase/me/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.frase.me
 
+import android.os.Bundle
+import androidx.core.view.WindowCompat
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    // Habilita edge-to-edge para compatibilidade com Android 15 e SDK 35
+    WindowCompat.setDecorFitsSystemWindows(window, false)
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
@@ -19,4 +27,4 @@ class MainActivity : ReactActivity() {
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
-} 
+}
